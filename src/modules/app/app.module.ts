7de57fbbcc79b92f3c6ff263547150606862ab81
zolboo-gtn <e.zolboo@gtn.co.jp>
2022-05-07 22:39:55 +0800

    miscs

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,17 +1,19 @@
 import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 
 import { logger } from "common/middlewares";
+import { AuthModule } from "modules/auth";
+import { UsersController, UsersModule } from "modules/users";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(logger).forRoutes();
+    consumer.apply(logger).forRoutes(UsersController);
   }
 }
