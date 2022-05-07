8a6d437164949f7ba081d319795fa02a5adbf61c
zolboo-gtn <e.zolboo@gtn.co.jp>
2022-05-08 02:39:00 +0800

    miscs

--- src/modules/attendances/attendances.module.ts
+++ src/modules/attendances/attendances.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 
+import { PrismaModule } from "modules/prisma";
 import { AttendancesService } from "./attendances.service";
 import { AttendancesController } from "./attendances.controller";
 
 @Module({
+  imports: [PrismaModule],
+  exports: [AttendancesService],
   controllers: [AttendancesController],
   providers: [AttendancesService],
 })
--- src/modules/prisma/prisma.module.ts
+++ src/modules/prisma/prisma.module.ts
@@ -3,7 +3,7 @@ import { Module } from "@nestjs/common";
 import { PrismaService } from "./prisma.service";
 
 @Module({
-  providers: [PrismaService],
   exports: [PrismaService],
+  providers: [PrismaService],
 })
 export class PrismaModule {}
