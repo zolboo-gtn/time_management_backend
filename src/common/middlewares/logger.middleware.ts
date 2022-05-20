a7fbdd9077d0c2bcb73d31bfd9284ac2b91f18fa
zolboo-gtn <e.zolboo@gtn.co.jp>
2022-05-21 02:21:46 +0800

    minor change

--- src/common/middlewares/logger.middleware.ts
+++ src/common/middlewares/logger.middleware.ts
@@ -4,12 +4,10 @@ import { Request, Response, NextFunction } from "express";
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log("Request...");
     next();
   }
 }
 
 export function logger(req: Request, res: Response, next: NextFunction) {
-  // console.log(`Request...`);
   next();
 }
